f2491896e68ab3ff0ddcd6ed991543ed9206df8c
swapnacv21 <swapnavenu116@gmail.com>
2024-08-05 16:09:46 +0530

    data types

---
+++ demo.py
@@ -0,0 +1,57 @@
+print("Hello, Welcome")
+print("Nice To Meet You")
+print('THANK YOU')
+print('one')
+roll_no = 14
+print("roll no:",roll_no)
+name = "Jibin Aji"
+print("Name :",name)
+weight = 68.5
+print("weight:",weight)
+address="nirappill"
+print("address",address)
+
+# data types
+
+# 1) Numeric data type: 
+# integer:
+roll_no=10
+print("roll no:",roll_no ,type(roll_no))
+
+
+# float:
+weight=55.5
+print("weight:",weight,type(weight))
+
+# complex:
+a=10+3j
+print("a:",a,type(a))
+
+# 2) String data Type:
+name="Sandra"
+print("name:",name,type(name))
+
+# 3) Boolean data type::
+a=True
+b=False
+print("a:",a,type(a))
+print('b:',b,type(b))
+
+# 4) Sequence data type:
+
+# list:
+list=["Jibin","Sandra","Swapna"]
+print("list:",list,type(list))
+
+# tuple:
+tuple=("Jibin","Sandra","Swapna")
+print("tuple:",tuple,type(tuple))
+
+# 5)Set Data Type:
+name={"anu","shyam","david"}
+print("name:",name,type(name))
+
+# 6) Mapping Data Type:
+data={'name':'Jibin'}
+print("data",data,type(data))
+
