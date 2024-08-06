842b32e2d6aa6cd1c96f2bc1ba0e87a4fd4d4eed
swapnacv21 <swapnavenu116@gmail.com>
2024-08-06 15:34:56 +0530

    input function

---
+++ demo1.py
@@ -0,0 +1,37 @@
+# input function & typecasting:
+
+# name = input("Enter Your Name : ")
+# age = int(input("Enter Your Age : "))
+# weight = float(input("Enter Your Weight : "))
+# print("Name",name)
+# print("Age",age)
+# print("Weight",weight)
+
+# typecasting without input function:
+'''a="abcd"
+print("a",type(a),a)'''
+"""b=str(a)
+print("b",type(b),b)
+c=int(a)
+print("c",type(c),c)"""
+
+# \n  :
+
+print("Hello \nWelcome \nTo \All")
+
+# \t :
+
+print("Nice \tTo \t\tMeet \tYou")
+
+# multiline string :
+print("""Happy
+To
+See
+You""")
+
+# f string :
+
+item=input("Enter the item")
+price=int(input("Enter the price"))
+print("It's an" ,item,"It's price is",price,"rs")
+print(f"It's an {item} It's price is {price} rs")
