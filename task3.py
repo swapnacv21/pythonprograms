0edc3550e7e9b0221e437b25fef3907d0a640065
swapnacv21 <swapnavenu116@gmail.com>
2024-08-12 16:04:26 +0530

    tasks usng python

---
+++ task3.py
@@ -0,0 +1,58 @@
+# question 1 Salary:
+
+# salary=int(input("enter your salary"))
+# year=int(input("enter year of experience"))
+# if (year>=5):
+#     print("your net bonus amount is",salary*0.05)
+#     print("your net bonus amount is",(salary*0.05)+salary)
+# else:
+#     print("no bonus") 
+
+# question 2 electricity bill:
+
+# unit=int(input("Enter the number of unit used"))
+# if (unit<=100):
+#     print("No charge")
+
+# elif(100<unit<=200):
+#     print("charge is",(unit-100)*5)
+# else:
+#     # cost=(unit-200)*10
+#     # print("charge is",(cost+500))
+#     print("charge is",((unit-200)*10)+500)
+
+# question 3 Days:
+
+# number=int(input("Enter a Number"))
+# if (number==1):
+#     print("Sunday")
+# elif (number==2):
+#     print("Monday")
+# elif (number==3):
+#     print("Tuesday")
+# elif (number==4):
+#     print("Wednesday")
+# elif (number==5):
+#     print("Thursday")
+# elif (number==6):
+#     print("Friday")
+# elif (number==7):
+#     print("Saturday")
+# else:
+#     print("invalid")
+
+# question 4 city and monument:
+# city=input("Enter your city")
+# city=city.lower()
+# if(city=="delhi"):
+#     print("Red Fort")
+# elif(city=="agra"):
+#     print("Taj Mhal")
+# elif(city=="jaipur"):
+#     print("Jal Mahal")
+# else:
+#     print("invalid")
+
+# question 5 last digit divisible by 3 or not:
+number=int(input("enter a number"))
+if
