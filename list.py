a6f43f3ffda937b3859938ee716cd24023373749
swapnacv21 <swapnavenu116@gmail.com>
2024-08-19 16:08:59 +0530

    list and list methods

---
+++ list.py
@@ -0,0 +1,67 @@
+# empty list:
+
+# l=[]
+# print(len(l))
+
+# output: 0
+
+# -------------------------
+
+# lenth of a list:
+
+# l=[1,2,'abc',10.5,1]
+# print(len(l))
+
+# output: 5
+
+# ---------------------------
+
+# l=[1,2,3,'abc',2.5,1]
+# if 'abc' in l:
+#     print("yes")
+# else:
+#     print("no")
+
+# output: yes
+
+# ----------------------------
+
+# l=[1,2,3,'abc',2.5,1]
+# for i in l:
+#     print(i)
+
+# output: 
+# 1
+# 2
+# 3
+# abc
+# 2.5
+# 1
+
+# ----------------------------
+
+# l=[1,2,3,'xyz',6.5]
+# a=input("enter a value")
+# if a in l:
+#     print("yes")
+# else:
+#     print("no")
+
+# ----------------------------
+
+# update a value present in a list:
+
+# list=[1,2,'xyz',3.5,4,5]
+# print(list[0])
+# print(list[5])
+# list[0]=20
+# print(list)
+# print(list[0])
+
+# output:
+# 1
+# 5
+# [20, 2, 'xyz', 3.5, 4, 5]
+# 20
+
+# ------------------------------
---
+++ listmethods.py
@@ -0,0 +1,108 @@
+# value adding methods:
+
+# 1)append() :
+
+# l=[]
+# l.append(5)
+# l.append(10)
+# l.append('abcd')
+# l.append([20,30,40])
+# print(l)
+
+# output: [5, 10, 'abcd', [20, 30, 40]]
+
+# 2)extend()  :
+
+# l=[]
+# l.append(5)
+# l.append(10)
+# l.append('abcd')
+# l.append([20,30,40])
+# l.extend([1,2,3])
+# print(l)
+
+# outpt: [5, 10, 'abcd', [20, 30, 40], 1, 2, 3]
+
+# 3)insert()  :
+
+# l=[]
+# l.append(5)
+# l.append(10)
+# l.append('abcd')
+# l.append([20,30,40])
+# l.extend([1,2,3])
+# l.insert(2,"hello")
+# print(l)
+
+# output: [5, 10, 'hello', 'abcd', [20, 30, 40], 1, 2, 3]
+
+
+# value removing methods:
+
+# 1)clear() :
+
+# l=[10,20,30,'abc']
+# l.clear()
+# print(l)
+
+#output: []
+
+# 2)pop() :
+# example 1:
+# ------------
+
+# l=[10,20,30,'abc']
+# l.pop()
+# print(l)
+
+# output: [10, 20, 30]
+
+# example 2:
+# ------------
+# l=[10,20,30,'abc']
+# l.pop(2)
+# print(l)
+
+# output:[10, 20, 'abc']
+
+# 3)remove() :
+
+# l=[10,20,30,'abc']
+# l.remove(20)
+# print(l)
+
+# output: [10, 30, 'abc']
+
+# ----------------------------
+
+# sort() :
+
+# l=[10,60,30,40]
+# l.sort()
+# print(l)
+
+# output: [10, 30, 40, 60]
+
+
+# reverse() :
+
+# l=[10,15,20,'xyz']
+# l.reverse()
+# print(l)
+
+# output: ['xyz', 20, 15, 10]
+
+# index() :
+
+# l=[10,20,"hello"]
+# print(l.index('hello'))
+
+# output: 2
+
+# count() :
+
+# l=[2,4,6,6,'hai']
+# print(l.count(6))
+
+# output: 
+# 2
