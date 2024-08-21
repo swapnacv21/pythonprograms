2043ce5baa7a6533ad8f48d0a471bf779929ee46
swapnacv21 <swapnavenu116@gmail.com>
2024-08-21 15:50:26 +0530

    list tasks

--- task7.py
+++ task7.py
@@ -1,12 +1,22 @@
 # question 1:
 
-# sum of odd umbers and even numbers:
+# sum of odd numbers and even numbers:
 
-l=[1,2,3,4,5,6]
-a=l[0]
-for i in l:
-    if i%2==1:
-        print(i)
+# l=[1,2,3,4,5,6]
+# a=l[0]
+# s=0
+# b=0
+# for i in l:
+#     if i%2==1:
+#         s+=i
+#     elif i%2==0:
+#         b+=i
+# print("sum of odd numbers=",s)
+# print("sum of even numbers=",b)
+
+# output:
+# sum of odd numbers= 9
+# sum of even numbers= 12
 
 # question 2:
 
@@ -14,8 +24,17 @@ for i in l:
 
 # l=[10,11,5.8,'abc']
 
+
+
 # question 3:
 
 # divisible by 2 and square of that numbers:
 
-# l=[1,2,5,8,9,11,16,12]
+l=[1,2,5,8,9,11,16,12]
+a=[0]
+b=0
+for i in l:
+    if i%2==0:
+        b=i
+        print(b)
+        
