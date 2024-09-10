2b43d8f641860e468ae58bb67dfa802fdf9c9440
swapnacv21 <swapnavenu116@gmail.com>
2024-09-10 13:30:13 +0530

    function arguments and tasks

---
+++ arguments.py
@@ -0,0 +1,41 @@
+# Types of arguments:
+'''----------------------'''
+
+
+# 1) Positional Argumnets (no of parameters = no of arguments)
+'''---------------------------------'''
+
+'''example:'''
+
+# def dtls(name,age):            ----> parameters -  used to collect values, given when creating a function
+#     print('name= ',name)
+#     print('age= ',age)
+# dtls('jibin',21)               ----> arguments - given when calling function
+# dtls('swapna',20)
+# dtls(26,'sandra')
+
+'''output:
+
+name=  jibin
+age=  21
+name=  swapna
+age=  20
+name=  26
+age=  sandra'''
+
+
+# 2)Keyword arguments (variables are used in argument)
+'''-------------------'''
+
+'''example:'''
+
+# def dtls(name,age):
+#     print("name = ",name)
+#     print("age = ",age)
+# dtls(age=22,name='alex')
+
+
+''' output:
+
+name =  alex
+age =  22'''
---
+++ functioncalculator.py
@@ -0,0 +1,42 @@
+# program: basic calculator
+
+def numbers():
+    a=int(input("Enter 1st number"))
+    b=int(input("Enter 2nd number"))
+    return a,b
+def add():
+    a,b=numbers()
+    print(a+b)
+def sub():
+    a,b=numbers()
+    print(a-b)
+def mul():
+    a,b=numbers()
+    print(a*b)
+def div():
+    a,b=numbers()
+    print(a/b)
+def mod():
+    a,b=numbers()
+    print(a%b)
+while True:
+    print('''
+1.Addition
+2.Subtraction
+3.Multiplication
+4.Division
+5.Modulus
+6.Exit''')
+    choice=int(input("enter your choice"))
+    if choice==1:
+        add()
+    elif choice==2:
+        sub()
+    elif choice==3:
+        mul()
+    elif choice==4:
+        div()
+    elif choice==5:
+        mod()
+    elif choice==6:
+        break
