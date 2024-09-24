4390833eddcc5afd6793d417665938323967dd5d
swapnacv21 <swapnavenu116@gmail.com>
2024-09-24 13:36:15 +0530

    file handling

--- filehandling/filehandling.py
+++ filehandling/filehandling.py
@@ -1,4 +1,37 @@
-f=open('pythonprograms/filehandling/demo.txt','r')
-f.write("Hello, Welcome to all")
-f.write('2024')
-print(f.read())
+f=open('pythonprograms/filehandling/demo.txt','a')
+
+'''# writing a content when creating a file'''
+
+# f.write("Hello, Welcome to all") 
+# f.write('2024')
+
+'''Read contents in a file'''
+
+# print(f.read())
+# a=f.read(5)
+# f.seek(0) ------------------------> to change cursor point
+# b=f.read()
+# print(a)
+# print('_'*20)
+# print(b)
+
+'''readline()'''
+
+# a=f.readline(5)
+# b=f.readline()
+# print(a)
+# print(b)
+
+
+# c=f.readlines()
+# print(c)
+
+# d=f.readlines()
+# f.seek(0)
+# for i in range(len(d)):
+#     a=f.readline().strip()
+#     print(a[::-1])
+    # print(a)
+
+f.write('happy birthday \n')
+f.write('namaste')
---
+++ filehandling/multiplicationtable.py
@@ -0,0 +1,6 @@
+# f=open('pythonprograms/filehandling/mult.txt','w')
+# number=int(input("Enter a number : "))
+# for i in range(1,11):
+#     # print(i,'*',number,'=',i*number)
+#     f.write(str(i)+'*'+str(number)+'='+str(i*number)+'\n')
+
