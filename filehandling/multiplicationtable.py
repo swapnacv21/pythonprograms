250d4fa92f74e02de3dd4fe969401f6a0eff84fe
swapnacv21 <swapnavenu116@gmail.com>
2024-09-25 10:51:22 +0530

    file handling

--- filehandling/multiplicationtable.py
+++ filehandling/multiplicationtable.py
@@ -4,3 +4,17 @@
 #     # print(i,'*',number,'=',i*number)
 #     f.write(str(i)+'*'+str(number)+'='+str(i*number)+'\n')
 
+
+# f=open('pythonprograms/filehandling/mult.txt','w')
+# number=int(input("Enter a number : "))
+# for i in range(1,11):
+#     print(i,'*',number,'=',i*number)
+#     f.write(str(i)+'*'+str(number)+'='+str(i*number)+'\n')
+
+f=open('pythonprograms/filehandling/mult1.txt','w')
+number=int(input("Enter a number : "))
+for i in range():
+    for j in range(1,11):
+        # print(print(j,'*',number,'=',j*number))
+        f.write(str(i)+'*'+str(number)+'='+str(i*number)+'\n')
+    print()
