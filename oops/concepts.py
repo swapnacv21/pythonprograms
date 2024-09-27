5894a9d37d7a870ea0871c98ba71f2785e92e3cf
swapnacv21 <swapnavenu116@gmail.com>
2024-09-27 13:32:46 +0530

    object oriented programming

---
+++ oops/concepts.py
@@ -0,0 +1,31 @@
+'''1) class  '''
+# syntax:
+
+''' class class_name:
+        statements '''
+# ------------------------
+
+'''2) object '''
+# syntax:
+
+''' var_name = class_name '''
+# ------------------------
+
+'''3) method '''
+# syntax:
+'''def method_name:
+        statements '''
+
+# example:
+
+class synnefo:                #------> class
+    def python():             #------> method
+        print("Python in synnefo")
+    def php():
+        print("php in synnefo")
+
+std=synnefo                   #------> object
+std.python()
+std.php()
+std1=synnefo
+std1.python()
---
+++ oops/example1bank.py
@@ -0,0 +1,67 @@
+# class Bank:
+#     def __init__(self):
+#         self.bal=0
+#     def deposit(self,amt):
+#         self.bal+=amt
+#         print("Amount added")
+#     def withdraw(self,amt):
+#         self.bal-=amt
+#         print("Amount withdraw")
+#     def display(self):
+#         print("remaining balance : ",self.bal)
+# user1=Bank()
+# user1.deposit(10000)
+# user1.withdraw(2000)
+# user1.display()
+
+
+'''output:
+Amount added
+Amount withdraw
+remaining balance :  8000'''
+
+# user2=Bank()
+# user2.deposit(1000)
+# user2.withdraw(4000)
+# user2.display()
+'''
+output:
+Amount added
+Amount withdraw
+remaining balance :  -3000'''
+
+# ---------------------------------------------------------------------------------------
+
+'''sample program using types of arguments:'''
+# ------------------------------------------
+
+'''1)positional argument:'''
+
+# class Demo:
+#     def __init__(self,a):
+#         print("a=",a)
+# obj=Demo(10)
+
+# output: a= 10
+'''2) Keyword argument : '''
+
+# class Demo:
+#     def __init__(self,a,b):
+#         print("1st=",a)
+#         print("2nd=",b)
+# obj=Demo(a=10,b=12)
+
+# output:
+# 1st= 10
+# 2nd= 12
+'''3) Arbitary argument :'''
+
+# class Demo:
+#     def __init__(self,*a):
+#         print("a=",a)
+# obj=Demo(500,1000)
+
+# output: a= (500, 1000)
+
+'''Arbitory keyword argument : '''
+'''Default argument : '''
